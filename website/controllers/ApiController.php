0521948077af2c84c1edecfc39e4d7130f8ebc11
Ahmad Somadi <ahmad.gerbawani89@gmail.com>
2015-06-07 21:23:14 +0700

    Update ApiController and add ApiBackup

--- website/controllers/ApiController.php
+++ website/controllers/ApiController.php
@@ -17,6 +17,342 @@ class ApiController extends Zend_Rest_Controller {
                 ->initContext('json');
 	}
 	
+	public function dietCategoriesAction()
+	{
+		
+		$id = $this->_getParam('id');
+		
+		$category = new Object\DietCategories\Listing();
+		$category->setOrderKey('name');
+		$category->setOrder('ASC');	    
+    		
+		$array = array();
+		$i = 0;
+		
+		if($this->_request->isPost())
+		{
+			if($id != '') {
+			    $category->setCondition('o_id = ?', $id);
+			}
+		}
+		else if($this->_request->isGet())
+		{
+			if($id != '') {
+			    $category->setCondition('o_id = ?', $id);
+			}
+		}
+		
+		foreach($category as $entry)
+		{
+			$arr = array();
+
+			$arr['id'] = $entry->getO_Id();
+			$arr['name'] = $entry->getName();
+			$arr['image_url'] = $entry->getImage();
+			
+			array_push($array, $arr);
+		}
+		
+		$json_cat = $this->_helper->json($array);
+		$this->sendResponse($arr);
+	}
+	
+	public function menuAction()
+	{
+		$params = $this->_getParam('id');
+		
+		$menu = new Object\Menu\Listing();
+		
+		$array = array();
+		$i = 0;
+				
+		if($params != '')
+		{
+			if($this->_request->isPost())
+			{
+				$menu->setCondition('o_id = ?', $params);
+			}
+			else if($this->_request->isGet())
+			{
+				$menu->setCondition('o_id = ?', $params);
+			}
+		}
+		
+		foreach($menu as $entry)
+		{
+			$valid = 1;
+			$arr = array();
+			
+			$arr['o_key'] = $entry->o_key;
+			$arr['id'] = $entry->getO_Id();
+			$arr['name'] = $entry->getName();
+			$arr['price'] = $entry->getPrice();
+			$arr['currency'] = $entry->getCurrency();
+			$arr['rating'] = $entry->getRating();
+			$arr['halal'] = $entry->getHalal();
+			$arr['description'] = ($entry->getDescription() == null) ? $entry->getDescription() : '';
+
+			// get diet categories
+			$x = 0;
+			if(count($entry->getCategories()) > 0)
+			{
+				foreach($entry->getCategories() as $category)
+				{
+					$arr['category'][$x]['id'] = $category->getO_Id();
+					$arr['category'][$x]['name'] = $category->getName();
+					$arr['category'][$x]['image'] = $_SERVER['REQUEST_SCHEME'] . '://' .  $_SERVER['HTTP_HOST'] . $category->image->path . $category->image->filename;;
+					$x++;
+				}			
+			}
+			else
+			{
+				$valid = 0;
+			}
+											    
+			// get restaurant relation data
+			if($entry->getRestaurants()->getO_Id() != null)
+			{
+				$arr['restaurants']['id'] = $entry->getRestaurants()->getO_Id();
+				$arr['restaurants']['name'] = $entry->getRestaurants()->getName();
+				$arr['restaurants']['address'] = $entry->getRestaurants()->getAddress();
+				$arr['restaurants']['location']['latitude'] = $entry->getRestaurants()->getLocation()->getLatitude();
+				$arr['restaurants']['location']['longitude'] = $entry->getRestaurants()->getLocation()->getLongitude();
+				//$arr['restaurants']['city'] = $entry->getRestaurants()->getCity()->getName();
+				//$arr['restaurants']['state'] = $entry->getRestaurants()->getCity()->getState()->getName();
+			}
+			else
+			{
+				$valid = 0;
+			}
+
+			// get image thumbnail
+			if($entry->thumb_image != null)
+			{
+				$arr['thumb_image'] = $_SERVER['REQUEST_SCHEME'] . '://' .  $_SERVER['HTTP_HOST'] . $entry->thumb_image->path . $entry->thumb_image->filename;					
+			}
+			else
+			{
+				$arr['thumb_image'] = null;
+			}
+		    
+			// get ingredients menu
+			$x = 0;
+			if(count($entry->ingredients->items) > 0)
+			{
+				foreach($entry->ingredients->items as $ingredient)
+				{
+					$arr['ingredients'][$x] = $ingredient->ingredient;
+					$x++;
+				}
+			}
+			else
+			{
+				$arr['ingredients'][$x] = "-";					
+			}
+		    
+			// get image collection menu
+			$x = 0;
+			if(count($entry->images->items) > 0)
+			{
+			    foreach($entry->images->items as $image)
+			    {
+				    $arr['images'][$x] = $_SERVER['REQUEST_SCHEME'] . '://' .  $_SERVER['HTTP_HOST'] . $image->image->path . $image->image->filename;
+				    $x++;
+			    }
+			}
+
+			$arr['timestamp_creation'] = $entry->getCreationDate();
+			$arr['creation_date'] = date('Y-m-d', $entry->getCreationDate());
+			
+			if($valid)
+				array_push($array, $arr);
+				
+		}
+				
+		$json_menu = $this->_helper->json($array);
+		$this->sendResponse($json_menu);
+	}
+	
+	public function restaurantsAction()
+	{	    
+		$params = $this->_getParam('id');
+		
+		$resto = new Object\Restaurants\Listing();
+		
+		$arrays = array();
+		$i = 0;
+					
+		if($params != '')
+		{
+			if($this->_request->isPost())
+			{
+				if($params!='') {
+					    $resto->setCondition('o_id = ?', $params);
+				    }
+			}
+			else if($this->_request->isGet())
+			{
+				if($params!='') {
+					$resto->setCondition('o_id = ?', $params);
+				}
+			}
+		}
+		
+		foreach($resto as $entry)
+		{
+			$valid = 1;
+			$array = array();
+			
+			$array['o_key'] = $entry->o_key;
+			$array['id'] = $entry->getO_Id();
+			$array['name'] = $entry->getName();
+
+			// query full menu
+			$menu = new Object\Menu\Listing();	
+			$menu->setCondition("restaurants__id = ". $entry->getO_Id());
+			$menu->setOrderKey('name');
+			$menu->setOrder('ASC');
+
+			if($menu->count() > 0)
+			{
+				$average_rating = 0;
+				$j = 0;			
+				foreach($menu as $m)
+				{
+					$array['full_menu'][$j]['id'] = $m->getO_Id();
+					$array['full_menu'][$j]['name'] = $m->getName();
+					$array['full_menu'][$j]['price'] = $m->getPrice();
+					$array['full_menu'][$j]['currency'] = $m->getCurrency();
+					$array['full_menu'][$j]['rating'] = $m->getRating();
+					$array['full_menu'][$j]['halal'] = $m->getHalal();
+		    
+					if($m->thumb_image != null)
+					{
+						$array['full_menu'][$j]['thumb_image'] = $_SERVER['REQUEST_SCHEME'] . '://' .  $_SERVER['HTTP_HOST'] . $m->thumb_image->path . $m->thumb_image->filename;						
+					}
+					else
+					{
+						$array['full_menu'][$j]['thumb_image'] = null;						
+					}
+					
+					$average_rating += $m->getRating();
+					
+					$j++;				
+				}
+				
+				$total_menu = $j;
+				$rating_restaurant = $average_rating / $total_menu;			    
+			}
+			else
+			{
+				$valid = 0;
+			}
+			
+			// query top rated menu
+			$limit = (j> 15) ? ceil(0.2 * $total_menu) : 3;
+			$menu = new Object\Menu\Listing();
+			$menu->setCondition("restaurants__id = ". $entry->getO_Id());
+			$menu->setOrderKey('rating');
+			$menu->setOrder('DESC');
+			$menu->setLimit($limit);
+			    
+			if($menu->count() > 0)
+			{
+			    $j = 0;			
+			    foreach($menu as $m)
+			    {
+				$array['recomended_menu'][$j]['id'] = $m->getO_Id();
+				$array['recomended_menu'][$j]['name'] = $m->getName();
+				$array['recomended_menu'][$j]['price'] = $m->getPrice();
+				$array['recomended_menu'][$j]['currency'] = $m->getCurrency();
+				$array['recomended_menu'][$j]['rating'] = $m->getRating();
+				$array['recomended_menu'][$j]['halal'] = $m->getHalal();
+	    
+				if($m->thumb_image != null)
+				{
+					$array['recomended_menu'][$j]['thumb_image'] = $_SERVER['REQUEST_SCHEME'] . '://' .  $_SERVER['HTTP_HOST'] . $m->thumb_image->path . $m->thumb_image->filename;						
+				}
+				else
+				{
+					$array['recomended_menu'][$j]['thumb_image'] = null;						
+				}
+								
+				$j++;				
+			    }
+			}
+			else
+			{
+				$valid = 0;
+			}
+
+			$x = 0;
+			if(count($entry->imageCollection->items) > 0)
+			{
+				foreach($entry->imageCollection->items as $image)
+				{
+					$array['image_collection'][$x] = $_SERVER['REQUEST_SCHEME'] . '://' .  $_SERVER['HTTP_HOST'] . $image->image->path . $image->image->filename;
+					$x++;
+				}
+			}
+			else
+			{
+				$array['image_collection'][$x] = null;
+			}
+		    
+			$x = 0;
+			if(count($entry->getOperationHour()->items) > 0)
+			{
+				foreach($entry->getOperationHour() as $hour)
+				{
+					$array['operation'][$x] = $hour->getOperationDay() . ' ' .  $hour->getOperationHour() . '\n';
+					$x++;
+				}
+			}
+			else
+			{
+				$array['image_collection'][$x] = 'No information';
+			}
+			
+			if($entry->profileImage != null)
+			{
+				$array['profile_image'] = $_SERVER['REQUEST_SCHEME'] . '://' .  $_SERVER['HTTP_HOST'] . $entry->profileImage->path . $entry->profileImage->filename;				
+			}
+			else
+			{
+				$array['profile_image'] = null;
+			}
+		    
+			$array['description'] = ($entry->getDescription() != null) ? $entry->getDescription() : 'No information';
+			$array['phone_number'] = ($entry->getPhoneNumber() != null) ? $entry->getPhoneNumber() : 'No information';
+			$array['website_url'] = ($entry->getWebsiteUrl() != null) ? $entry->getWebsiteUrl() : 'No information';
+			$array['email'] = ($entry->getEmail() != null) ? $entry->getEmail() : 'No information';
+			$array['zip_code'] = $entry->getZipCode();
+			$array['address'] = ($entry->getAddress() != null) ?  ($entry->getAddress() != null) : '';
+
+			$array['city'] = ($entry->getCity()->getName() != null) ? $entry->getCity()->getName() : '';
+			$array['state'] = ($entry->getCity()->getState()->getName() != null) ? $entry->getCity()->getState()->getName() : '';
+
+			$array['rating'] = round($rating_restaurant, 1);
+			
+			if($entry->getLocation()->getLongitude() != null && $entry->getLocation()->getLatitude() != null)
+			{
+				$array['location']['longitude'] = $entry->getLocation()->getLongitude();
+				$array['location']['latitude'] = $entry->getLocation()->getLatitude();				
+			}
+			else
+			{
+				$valid = 0;
+			}
+			 
+			if($valid)
+				array_push($arrays, $array);
+				
+		}		
+			
+		$json_resto = $this->_helper->json($arrays);
+		$this->sendResponse($json_resto);
+	}	
+
 	private function sendResponse($content) {
 		$this->getResponse()
 			->setHeader('Content-Type', 'json')
@@ -26,266 +362,34 @@ class ApiController extends Zend_Rest_Controller {
 	}
 	
 	public function indexAction()
-    {
-	    
-    }
+	{
+		
+	}
     
-    public function restaurantsAction()
-    {
-	    
-	    $params = $this->_getParam('id');
-	    
-	    $resto = new Object\Restaurants\Listing();
-	    
-	    $array = array();
-	    $i = 0;
-	    
-	    if($params!='')
-	    {
-		    
-		    if($this->_request->isPost())
-		    {
-			    if($params!='') {
-					$resto->setCondition('o_id = ?', $params);
-			  	}
-		    }
-		    else if($this->_request->isGet())
-		    {
-			    if($params!='') {
-				    $resto->setCondition('o_id = ?', $params);
-			    }
-		    }
-		    
-		    foreach($resto as $entry)
-		    {
-			    $array[$i]['id'] = $entry->getO_Id();
-			    $array[$i]['name'] = $entry->getName();
-			    $y = 0;
-			    foreach($entry->getCategory() as $category)
-			    {
-				    $array[$i]['categories'][$y]['id'] = $category->getO_Id();
-				    $array[$i]['categories'][$y]['name'] = $category->getName();
-				    $y++;
-			    }
-			    $array[$i]['description'] = $entry->getDescription();
-			    $array[$i]['phone_number'] = $entry->getPhoneNumber();
-			    $x = 0;
-			    foreach($entry->getOperationHour() as $hour)
-			    {
-				    $array[$i]['operation'][$x]['day'] = $hour->getOperationDay();
-				    $array[$i]['operation'][$x]['hour'] = $hour->getOperationHour();
-				    $x++;
-			    }
-			    $array[$i]['website_url'] = $entry->getWebsiteUrl();
-			    $array[$i]['profile_image_url'] = $entry->getProfileImage();
-			    $array[$i]['email'] = $entry->getEmail();
-			    $array[$i]['address'] = $entry->getAddress();
-			    $array[$i]['city'] = $entry->getCity()->getName();
-			    $array[$i]['state'] = $entry->getCity()->getState()->getName();
-			    $array[$i]['zip_code'] = $entry->getZipCode();
-			    $array[$i]['location']['longitude'] = $entry->getLocation()->getLongitude();
-			    $array[$i]['location']['latitude'] = $entry->getLocation()->getLatitude();
-			    $array[$i]['timestamp_creation'] = $entry->getCreationDate();
-			    $array[$i]['creation_date'] = date('Y-m-d', $entry->getCreationDate());
-			    $i++;
-		    }
-	    
-	    }
-	    else {
-		    
-		    $array[0]['error'] = "Invalid ID";
-		    $array[0]['response_code'] = "403";
-		    
-	    }
-	    $json_resto = $this->_helper->json($array);
-	    $this->sendResponse($json_resto);
-    }
-    
-    public function menulistAction()
-    {
-	    $limit = 10;
-	    $page = (int) $this->_getParam('page') == '' ? 1 : $this->_getParam('page');
-	    
-	    $start = ($page * $limit) - $limit;
-	    
-	    $menu = new Object\Menu\Listing();
-	    $menu->setOffset($start);
-	    $menu->setLimit($limit);
-	    $menu->setOrderKey('o_creationDate');
-	    $menu->setOrder('desc');
-	    
-	    $arr = array();
-	    $i = 0;
-	    
-	    foreach($menu as $entry)
-	    {
-		    $arr[$i]['id'] = $entry->getO_Id();
-		    $arr[$i]['name'] = $entry->getName();
-		    $arr[$i]['restaurants']['id'] = $entry->getRestaurants()->getO_Id();
-		    $arr[$i]['restaurants']['name'] = $entry->getRestaurants()->getName();
-		    $arr[$i]['restaurants']['address'] = $entry->getRestaurants()->getAddress();
-		    $arr[$i]['restaurants']['location']['latitude'] = $entry->getRestaurants()->getLocation()->getLatitude();
-		    $arr[$i]['restaurants']['location']['longitude'] = $entry->getRestaurants()->getLocation()->getLongitude();
-		    $arr[$i]['restaurants']['city'] = $entry->getRestaurants()->getCity()->getName();
-		    $arr[$i]['restaurants']['state'] = $entry->getRestaurants()->getCity()->getState()->getName();
-		    
-		    $x = 0;
-		    foreach($entry->getRestaurants()->getCategory() as $category)
-		    {
-			    $arr[$i]['category'][$x]['id'] = $category->getO_Id();
-			    $arr[$i]['category'][$x]['name'] = $category->getName();
-			    $x++;
-		    }
-		    
-		    $arr[$i]['price'] = $entry->getPrice();
-		    $arr[$i]['currency'] = $entry->getCurrency();
-		    $arr[$i]['image_url'] = $entry->getImage();
-		    $arr[$i]['timestamp_creation'] = $entry->getCreationDate();
-		    $arr[$i]['creation_date'] = date('Y-m-d', $entry->getCreationDate());
-		    
-		    $i++;
-	    }
-	    
-	    if($this->_request->isGet())
-	    {
-	    	$json_menu = $this->_helper->json($arr);
-	    }
-	    else {
-		    $array = array();
-		    $array[0]['error'] = "Method Not Allowed";
-		    $array[0]['response_code'] = "405";
-		    $json_menu = $this->_helper->json($array);
-	    }
-	    $this->sendResponse($json_menu);
-    }
-    
-    public function menuAction()
-    {
-	    $params = $this->_getParam('id');
-	    
-	    $menu = new Object\Menu\Listing();
-	    
-	    $arr = array();
-	    $i = 0;
-	    
-	    if($params!='')
-	    {
-		    if($this->_request->isPost())
-		    {
-			    $menu->setCondition('o_id = ?', $params);
-		    }
-		    else if($this->_request->isGet())
-		    {
-				$menu->setCondition('o_id = ?', $params);
-		    }
-		    
-		    foreach($menu as $entry)
-		    {
-			    $arr[$i]['id'] = $entry->getO_Id();
-			    $arr[$i]['name'] = $entry->getName();
-			    $arr[$i]['restaurants_id'] = $entry->getRestaurants()->getO_Id();
-			    $arr[$i]['restaurants'] = $entry->getRestaurants()->getName();
-			    
-			    $x = 0;
-			    foreach($entry->getRestaurants()->getCategory() as $category)
-			    {
-				    $arr[$i]['category'][$x]['id'] = $category->getO_Id();
-				    $arr[$i]['category'][$x]['name'] = $category->getName();
-				    $x++;
-			    }
-			    
-			    $arr[$i]['image_url'] = $entry->getImage();
-			    $arr[$i]['currency'] = $entry->getCurrency();
-			    $arr[$i]['price'] = $entry->getPrice();
-			    $arr[$i]['description'] = $entry->getDescription();
-			    $arr[$i]['halal'] = $entry->getHalal();
-			    
-			    foreach($entry->getIngredients() as $ing)
-			    {
-				    $arr[$i]['ingredients'][] = $ing->getIngredient();
-			    }
-			    $arr[$i]['timestamp_creation'] = $entry->getCreationDate();
-			    $arr[$i]['creation_date'] = date('Y-m-d', $entry->getCreationDate());
-			    $i++;
-		    }
-	    }
-		else {
-			$arr[0]['error'] = 'Invalid ID';
-			$arr[0]['response_code'] = '403';
-		}
-	    
-	    $json_menu = $this->_helper->json($arr);
-	    $this->sendResponse($json_menu);
-    }
-    
-    public function categoriesAction()
-    {
-	    //$limit = 10;
-	    //$page = (int) $this->_getParam('page') == '' ? 1 : $this->_getParam('page');
-	    //
-	    //$start = ($page * $limit) - $limit;
-	    
-	    $id = $this->_getParam('id');
-	    
-	    $category = new Object\Categories\Listing();
-	    //$category->setOffset($start);
-	    //$category->setLimit($limit);
-	    
-	    $arr = array();
-	    $i = 0;
-	    
-	    if($this->_request->isPost())
-	    {
-		    if($id != '') {
-		    	$category->setCondition('o_id = ?', $id);
-		    }
-	    }
-	    else if($this->_request->isGet())
-	    {
-		    if($id != '') {
-		    	$category->setCondition('o_id = ?', $id);
-		    }
-	    }
-	    
-	    foreach($category as $entry)
-	    {
-		    $arr[$i]['id'] = $entry->getO_Id();
-		    $arr[$i]['name'] = $entry->getName();
-		    $arr[$i]['image_url'] = $entry->getImage();
-		    $arr[$i]['timestamp_creation'] = $entry->getCreationDate();
-		    $arr[$i]['creation_date'] = date('Y-m-d', $entry->getCreationDate());
-		    
-		    $i++;
-	    }
-	    
-	    $json_cat = $this->_helper->json($arr);
-	    $this->sendResponse($arr);
-    }
-    
-    public function headAction()
-    {
-	    
-    }
-    
-    public function getAction()
-    {
-	    $content = "getAction()";
-	    $this->sendResponse($content);
-    }
-    
-    public function postAction()
-    {
-	    
-    }
-    
-    public function putAction()
-    {
-	    
-    }
-    
-    public function deleteAction()
-    {
-	    
-    }
+	public function headAction()
+	{
+		
+	}
+	
+	public function getAction()
+	{
+		$content = "getAction()";
+		$this->sendResponse($content);
+	}
+	
+	public function postAction()
+	{
+		
+	}
+	
+	public function putAction()
+	{
+		
+	}
+	
+	public function deleteAction()
+	{
+		
+	}
 	
 }

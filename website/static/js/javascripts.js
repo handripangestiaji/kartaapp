1e1c008886c1f1ef02d9fa3e6a3080751e3ea576
Handri Pangestiaji <handri.pangestiaji@gmail.com>
2015-08-04 23:34:15 +0700

    fixing javascript

--- website/static/js/javascripts.js
+++ website/static/js/javascripts.js
@@ -2,10 +2,9 @@ $( document ).ready(function(){
 
     console.log( 'Document is ready, my lord.' );
     
-    email = $('#email').val();
-    
     $('#join').click(function(){
 	   
+       email = $('#email').val();
 	   $.ajax({
 		   type: 'POST',
 		   url: '/submit',
@@ -13,7 +12,7 @@ $( document ).ready(function(){
 	   }).done(function(response){
 		  
 		  if(response == 'success') {
-			  window.location.href('/thankyou');
+			  window.location.href = '/thankyou';
 		  }
 		   
 	   });
